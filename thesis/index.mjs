da293ea775fd39c1b8c675c4d2fa17b47adf88fa
Mauricio Mercado <mau@maumercado.com>
2023-07-17 17:19:41 -0700

    Consolidation of trades done

--- thesis/index.mjs
+++ thesis/index.mjs
@@ -1,24 +1,118 @@
+import fs from 'fs';
 import { fileURLToPath } from 'node:url';
-import { readFile } from 'node:fs/promises';
 import { randomUUID } from 'node:crypto';
+import { parse } from 'csv-parse';
 
+// Parsing functions
+const parseTradeQuantity = (row) => parseInt(row['Trade Quantity'], 10);
+const parseEntryPrice = (row) => parseFloat(row['Entry Price']);
+const parseExitPrice = (row) => parseFloat(row['Exit Price']);
+const parseMaxOpenQuantity = (row) => parseInt(row['Max Open Quantity'], 10);
+
+// Trade functions
+const createTrade = (row) => ({
+  tradeId: randomUUID(),
+  symbol: row['Symbol'],
+  tradeType: row['Trade Type'],
+  entryDateTime: row['Entry DateTime'],
+  entryPrice: parseEntryPrice(row),
+  tradeQuantity: parseTradeQuantity(row),
+  exitDateTime: null,
+  exitPrice: null,
+  maxOpenQuantity: parseMaxOpenQuantity(row),
+  steps: [],  // To store the additions or partial exits
+});
+
+const updateTrade = (trade, row) => {
+  const newMaxOpenQuantity = parseMaxOpenQuantity(row);
+  const action = newMaxOpenQuantity > trade.maxOpenQuantity ? 'Added to Trade' : 'Took Profits';
+
+  trade.tradeQuantity += parseTradeQuantity(row);
+  trade.maxOpenQuantity = newMaxOpenQuantity;
+  trade.steps.push({
+    dateTime: row['Exit DateTime'],
+    price: parseExitPrice(row),
+    quantity: parseTradeQuantity(row),
+    action: action
+  });
+
+  return trade;
+};
+
+const closeTrade = (trade, row) => {
+  trade.exitDateTime = row['Exit DateTime'];
+  trade.exitPrice = parseExitPrice(row);
+  trade.tradeQuantity += parseTradeQuantity(row);
+  trade.steps.push({
+    dateTime: row['Exit DateTime'],
+    price: parseExitPrice(row),
+    quantity: parseTradeQuantity(row),
+    action: 'Took Profits'
+  });
+
+  return trade;
+};
+
+// Consolidation function
+const consolidateTrades = (records) => {
+  let currentTrade = null;
+  const consolidatedTrades = [];
+
+  for (const row of records) {
+    if (row['Entry DateTime'].includes('BP')) {
+      if (currentTrade) {
+        consolidatedTrades.push(currentTrade);
+      }
+      currentTrade = createTrade(row);
+    } else if (row['Exit DateTime'].includes('EP')) {
+      currentTrade = closeTrade(currentTrade, row);
+      consolidatedTrades.push(currentTrade);
+      currentTrade = null;
+    } else if (currentTrade) {
+      currentTrade = updateTrade(currentTrade, row);
+    }
+  }
+
+  if (currentTrade) {
+    consolidatedTrades.push(currentTrade);
+  }
+
+  return consolidatedTrades;
+};
+
+// Main function
+const processTrades = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const parser = fs.createReadStream(filePath).pipe(parse({
+      columns: true,
+      delimiter: '\t',
+      relax_column_count: true
+    }));
+
+    const records = [];
+    parser.on('readable', () => {
+      let record;
+      while (record = parser.read()) {
+        records.push(record);
+      }
+    });
+
+    parser.on('end', () => {
+      resolve(consolidateTrades(records));
+    });
+
+    parser.on('error', (error) => {
+      reject(error);
+    });
+  });
+};
+
+// Usage
 const filePath = new URL('../data/demo.tsv', import.meta.url);
 const absolutePath = fileURLToPath(filePath);
 
-const file = await readFile(absolutePath, 'utf8');
-
-const headers = file.split('\n')[0].split('\t');
-const json = file.split('\n')
-  .reduce((acc, curr, i) => {
-    if (i === 0) return { ...acc }
-    const row = curr.split('\t');
-    const obj = row.reduce((acc, curr, j) => {
-      if (curr === '') return;
-      return { ...acc, [headers[j]]: curr }
-    }, {})
-    if (obj === undefined) return ({ ...acc });
-    return { ...acc, [randomUUID()]: obj }
-  }, {});
-
-  console.log(json)
-// up to here everything is a new trade, but is not taking into consideration adds into a trade.
+processTrades(absolutePath).then(consolidatedTrades => {
+  console.log(JSON.stringify(consolidatedTrades, null, 2));
+}).catch(error => {
+  console.error(error);
+});
