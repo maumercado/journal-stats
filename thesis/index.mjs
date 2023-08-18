133a3f9e46acac238b9e22fc0114e42d48c97012
Mauricio Mercado <mau@maumercado.com>
2023-08-18 16:51:07 -0700

    Add standard and time parsing

--- thesis/index.mjs
+++ thesis/index.mjs
@@ -1,84 +1,88 @@
-import fs from 'fs';
-import { fileURLToPath } from 'node:url';
-import { randomUUID } from 'node:crypto';
-import { parse } from 'csv-parse';
+import fs from 'fs'
+import { fileURLToPath } from 'node:url'
+import { randomUUID } from 'node:crypto'
+import { parse } from 'csv-parse'
 
 // Parsing functions
-const parseTradeQuantity = (row) => parseInt(row['Trade Quantity'], 10);
-const parseEntryPrice = (row) => parseFloat(row['Entry Price']);
-const parseExitPrice = (row) => parseFloat(row['Exit Price']);
-const parseMaxOpenQuantity = (row) => parseInt(row['Max Open Quantity'], 10);
+const parseTradeQuantity = (row) => parseInt(row['Trade Quantity'], 10)
+const parseEntryPrice = (row) => parseFloat(row['Entry Price'])
+const parseExitPrice = (row) => parseFloat(row['Exit Price'])
+const parseMaxOpenQuantity = (row) => parseInt(row['Max Open Quantity'], 10)
+const pnl = (row) => parseFloat(row['Profit/Loss (P)'])
+const parseTime = (dateTime) => new Date(dateTime.replace('  ', 'T').replace(/ EP|BP/, '').trim())
 
 // Trade functions
 const createTrade = (row) => ({
   tradeId: randomUUID(),
-  symbol: row['Symbol'],
+  symbol: row.Symbol,
   tradeType: row['Trade Type'],
-  entryDateTime: row['Entry DateTime'],
+  entryDateTime: parseTime(row['Entry DateTime']),
   entryPrice: parseEntryPrice(row),
   tradeQuantity: parseTradeQuantity(row),
+  pnl: pnl(row),
+  status: pnl(row) > 0 ? 'Win' : 'Lose',
   exitDateTime: null,
   exitPrice: null,
   maxOpenQuantity: parseMaxOpenQuantity(row),
-  steps: [],  // To store the additions or partial exits
-});
+  steps: [] // To store the additions or partial exits
+})
 
 const updateTrade = (trade, row) => {
-  const newMaxOpenQuantity = parseMaxOpenQuantity(row);
-  const action = newMaxOpenQuantity > trade.maxOpenQuantity ? 'Added to Trade' : 'Took Profits';
+  const newMaxOpenQuantity = parseMaxOpenQuantity(row)
+  const action = newMaxOpenQuantity > trade.maxOpenQuantity ? 'Added to Trade' : 'Took Profits'
 
-  trade.tradeQuantity += parseTradeQuantity(row);
-  trade.maxOpenQuantity = newMaxOpenQuantity;
+  trade.tradeQuantity += parseTradeQuantity(row)
+  trade.maxOpenQuantity = newMaxOpenQuantity
   trade.steps.push({
-    dateTime: row['Exit DateTime'],
+    dateTime: parseTime(row['Exit DateTime']),
     price: parseExitPrice(row),
     quantity: parseTradeQuantity(row),
-    action: action
-  });
+    action
+  })
 
-  return trade;
-};
+  return trade
+}
 
 const closeTrade = (trade, row) => {
-  trade.exitDateTime = row['Exit DateTime'];
-  trade.exitPrice = parseExitPrice(row);
-  trade.tradeQuantity += parseTradeQuantity(row);
+  trade.exitDateTime = parseTime(row['Exit DateTime'])
+  trade.exitPrice = parseExitPrice(row)
+  trade.tradeQuantity += parseTradeQuantity(row)
   trade.steps.push({
-    dateTime: row['Exit DateTime'],
+    dateTime: parseTime(row['Exit DateTime']),
     price: parseExitPrice(row),
     quantity: parseTradeQuantity(row),
     action: 'Took Profits'
-  });
+  })
 
-  return trade;
-};
+  return trade
+}
 
 // Consolidation function
 const consolidateTrades = (records) => {
-  let currentTrade = null;
-  const consolidatedTrades = [];
+  let currentTrade = null
+  const consolidatedTrades = []
 
   for (const row of records) {
     if (row['Entry DateTime'].includes('BP')) {
       if (currentTrade) {
-        consolidatedTrades.push(currentTrade);
+        consolidatedTrades.push(currentTrade)
       }
-      currentTrade = createTrade(row);
+      currentTrade = createTrade(row)
     } else if (row['Exit DateTime'].includes('EP')) {
-      currentTrade = closeTrade(currentTrade, row);
-      consolidatedTrades.push(currentTrade);
-      currentTrade = null;
+      currentTrade = closeTrade(currentTrade, row)
+      consolidatedTrades.push(currentTrade)
+      currentTrade = null
     } else if (currentTrade) {
-      currentTrade = updateTrade(currentTrade, row);
+      currentTrade = updateTrade(currentTrade, row)
     }
   }
 
   if (currentTrade) {
-    consolidatedTrades.push(currentTrade);
+    consolidatedTrades.push(currentTrade)
   }
 
-  return consolidatedTrades;
-};
+  return consolidatedTrades
+}
 
 // Main function
 export const processTrades = async (filePath) => {
@@ -86,28 +90,27 @@ export const processTrades = async (filePath) => {
     columns: true,
     delimiter: '\t',
     relax_column_count: true
-  }));
+  }))
 
-  let records = [];
+  const records = []
   for await (const record of parser) {
-    records.push(record);
+    records.push(record)
   }
 
-  return consolidateTrades(records);
-};
-
+  return consolidateTrades(records)
+}
 
 // Usage
-const filePath = new URL('../data/demo.tsv', import.meta.url);
-const absolutePath = fileURLToPath(filePath);
+const filePath = new URL('../data/demo.tsv', import.meta.url)
+const absolutePath = fileURLToPath(filePath)
 
-async function main() {
+async function main () {
   try {
-    const consolidatedTrades = await processTrades(absolutePath);
-    console.log(JSON.stringify(consolidatedTrades, null, 2));
+    const consolidatedTrades = await processTrades(absolutePath)
+    console.log(JSON.stringify(consolidatedTrades, null, 2))
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-main();
+main()
